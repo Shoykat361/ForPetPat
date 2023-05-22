47ced16ee43e9d51df918b6718cf790a448d78a0
Shoykat361 <shoykat15-11724@diu.edu.bd>
2023-05-22 18:04:09 +0600

    Ui design

---
+++ app/src/main/java/com/shoykat/forpetpat/Adapter/homeAdapter.kt
@@ -0,0 +1,34 @@
+package com.shoykat.forpetpat.Adapter
+
+import android.provider.ContactsContract.RawContacts
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.shoykat.forpetpat.RawData
+import com.shoykat.forpetpat.databinding.RowLayoutBinding
+
+class HomeAdapter(private val imageList: List<RawData>) : RecyclerView.Adapter<HomeAdapter.ImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = RowLayoutBinding.inflate(inflater, parent, false)
+        return ImageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val imageData = imageList[position]
+        holder.bind(imageData)
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+
+    inner class ImageViewHolder(private val binding: RowLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(imageData: RawData) {
+            binding.imageData = imageData
+            binding.executePendingBindings()
+        }
+    }
+}
